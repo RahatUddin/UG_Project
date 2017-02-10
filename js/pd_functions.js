8f9e9f06321a2547141084848691f8d0f1b68f98
RahatUddin <rahatu8@gmail.com>
2017-02-10 17:04:14 +0000

    Update pd_functions.js

--- js/pd_functions.js
+++ js/pd_functions.js
@@ -472,7 +472,7 @@ function Waves_PlayStopPd(){
 						Pd.send('i_Numerator', [1])
 						Pd.send('wavesRate', [0.05])
 						Pd.send('wavesDepth', [1])
-						Pd.send('wavesAmp_1', [0.5])//L
+						Pd.send('wavesAmp_1', [1])//L
 						Pd.send('wavesAmp_2', [0])//R
 						Pd.start()
 						document.getElementById("PlayStop").innerHTML = "Stop Sound Board";
